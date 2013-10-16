fb22b26a6d2166113814f77094ebdd515fea633f
Noel Welsh <noel@untyped.com>
2013-10-16 14:24:52 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FightForTheFuture::Application.config.secret_token = 'f51c5a4f693b6cf30f476ed05fbf5e41e46722e007afa12f08780a071fe00de4f1160cfd5bf1357f2d6fdb241084afff26dcb51aaa3dacfb5bf53f59762e63cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FightForTheFuture::Application.config.session_store :cookie_store, key: '_fight-for-the-future_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FightForTheFuture::Application.config.session_store :active_record_store
